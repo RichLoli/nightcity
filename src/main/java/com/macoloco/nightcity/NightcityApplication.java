06b6cca951a90160963267f8266380bdf1c1bb25
Aya <loligafeo@gmail.com>
2021-05-20 17:24:33 +0800

    仓库初始化

---
+++ src/main/java/com/macoloco/nightcity/NightcityApplication.java
@@ -0,0 +1,13 @@
+package com.macoloco.nightcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightcityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macoloco/nightcity/NightcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macoloco.nightcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightcityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
